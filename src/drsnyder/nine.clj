997255d90ca1e62b03be5add935faa2f9ef2556a
Damon Snyder <drsnyder@gmail.com>
2020-12-10 09:58:47 -0600

    Nine part two

--- src/drsnyder/nine.clj
+++ src/drsnyder/nine.clj
@@ -3,8 +3,8 @@
 
 (defn lines->partitions [lines preamble]
   (map vec
-       (partition (+ preamble 1) 1
-                  (map #(Integer/parseInt %) lines))))
+       (partition preamble 1
+                  (map #(bigint %) lines))))
 
 (defn find-first-failure [partitions]
   (loop [parts partitions]
@@ -19,5 +19,24 @@
 
 ; part one 552655238
 (defn part-one [lines preamble]
-  (let [parts (lines->partitions lines preamble)]
+  (let [parts (lines->partitions lines (+ preamble 1))]
     (find-first-failure parts)))
+
+(defn parts->seq-sums [parts]
+  (map (fn [part]
+         [(apply + part) part])
+       parts))
+
+(defn find-failure-sequence-by-preamble [lines preamble sum-target]
+  (let [parts (lines->partitions lines preamble)
+        sum-seqs (parts->seq-sums parts)]
+    (first (filter #(= (first %) sum-target) sum-seqs))))
+
+; 66770727 is too low; 70672245 is just right
+(defn part-two [lines preamble]
+  (let [target-sum (part-one lines preamble)]
+    (loop [seq-size 2]
+      (let [found (second (find-failure-sequence-by-preamble lines seq-size target-sum))]
+        (if found
+          [target-sum (+ (apply min found) (apply max found))]
+          (recur (+ seq-size 1)))))))
