ee4cf3b1a58f122d695a3216dc9400b4644f5e56
Damon Snyder <drsnyder@gmail.com>
2020-12-13 21:17:50 -0600

    Final solution for 10

--- src/drsnyder/ten.clj
+++ src/drsnyder/ten.clj
@@ -60,18 +60,44 @@
     (prn results)
     (partition-by identity results)))
 
+(defn split-paths [paths chain]
+  (map first
+       (filter #(not (nil? %))
+               (mapcat (fn [p]
+                         (let [s (drop (get-start-pos chain p) chain)]
+                           (map (comp first (partial next-path-with-step s)) [1 2 3])))
+                       paths))))
+(def split-paths-m (memoize split-paths))
+
+; OOM heap space
 (defn part-two-iter-2 [lines]
   (let [[_ device chain] (setup-chain lines)]
-    (loop [pos [(first chain)]
+    (loop [paths [(first chain)]
            finished 0]
-      (if (empty? pos)
+      (if (empty? paths)
         finished
-        (let [new-pos (map first
-                           (filter #(not (nil? %))
-                                   (mapcat (fn [p]
-                                             (let [s (drop (get-start-pos chain p) chain)]
-                                               (map (comp first (partial next-path-with-step s)) [1 2 3])))
-                                           pos)))
+        (let [new-pos (split-paths-m paths chain)
               new-finished (+ finished (count (filter #(= % device) new-pos)))
               next-round (filter #(not= % device) new-pos)]
           (recur next-round new-finished))))))
+
+(defn get-vec-pos [v pos]
+  (if (< pos 0)
+    0
+    (get v pos)))
+
+
+; 32396521357312
+; What an elegant solution. I had to get some help from reddit for this one.
+; https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gfnsrjv/?utm_source=reddit&utm_medium=web2x&context=3
+; https://hackernoon.com/google-interview-questions-deconstructed-the-knights-dialer-f780d516f029
+(defn part-two-iter-3 [lines]
+  (let [[_ _ chain] (setup-chain lines)
+        counts (vec (concat (list 1) (take (last chain) (cycle (list 0)))))]
+    (last (reduce (fn [v pos]
+                    (prn v)
+                    (assoc v pos (+ (get-vec-pos v (- pos 3))
+                                    (get-vec-pos v (- pos 2))
+                                    (get-vec-pos v (- pos 1)))))
+                  counts
+                  (rest chain)))))
